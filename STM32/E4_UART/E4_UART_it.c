5f44041d1863fe583155edf038c8221dc392e69f
Korzhak <navchalkin@gmail.com>
2022-09-28 12:12:12 +0300

    added UART IT example

---
+++ STM32/E4_UART/E4_UART_it.c
@@ -0,0 +1,48 @@
+/*
+ * EXAMPLE 4
+ * ONLY INTERRUPTS UART
+ */
+
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+/* USER CODE END Includes */
+
+
+/* USER CODE BEGIN 0 */
+uint8_t UART1_rxBuffer[DATA_SIZE] = {0};
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    HAL_UART_Receive_IT(&huart1, UART1_rxBuffer, DATA_SIZE);
+    for (int i=0; i<DATA_SIZE; i++)
+    {
+        printf("%c", (char) UART1_rxBuffer[i]);
+    }
+    printf("\r\n");
+}
+/* USER CODE END 0 */
+
+
+int main(void)
+{
+    /* USER CODE BEGIN 2 */
+    HAL_UART_Receive_IT(&huart1, UART1_rxBuffer, DATA_SIZE);
+    /* USER CODE END 2 */
+
+    while (1)
+    {
+
+    }
+}
+
+
+
+/* USER CODE BEGIN 4 */
+int _write ( int file, char *ptr, int len )
+{
+  int i = 0;
+  for ( i = 0; i < len; i++ )
+    ITM_SendChar ( (*ptr++ ) );
+  return len;
+}
+/* USER CODE END 4 */
